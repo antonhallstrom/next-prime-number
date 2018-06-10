03f598ca247b967de2225be3d8da787992acc52a
Anton Hallstrom <antonhallstrom@live.se>
2018-06-10 20:59:18 +0200

    fixup! feat: create nextPrime

--- next-prime.js
+++ next-prime.js
@@ -1,22 +1,20 @@
 function isPrime(n) {
-    for (let i = 2; i < n; i++) {
-      if (n % i === 0) {
-          return false
-      }
-    }
-     return n > 1
-  }
-  
-  function nextPrime(n) {
-  
-    for (let i = n; i < 2 * n; i++) {
-      let next = i + 1
-  
-      if (isPrime(next)) {
-        return next
-      }
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+        return false
     }
   }
-  
-  module.exports = nextPrime
-  
+   return n > 1
+}
+
+function nextPrime(n) {
+  for (let i = n; i < 2 * n; i++) {
+    let next = i + 1
+
+    if (isPrime(next)) {
+      return next
+    }
+  }
+}
+
+module.exports = nextPrime
